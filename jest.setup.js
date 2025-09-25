96435b9be11c867cdca964875ca263697666c673
Eliezio Oliveira <eliezio@pm.me>
2025-09-25 15:45:21 +0100

    Add jest, supertest, and mongodb-memory-server

---
+++ jest.setup.js
@@ -0,0 +1,22 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongo;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri, { dbName: 'test' });
+});
+
+afterEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (const c of collections) {
+    await c.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
